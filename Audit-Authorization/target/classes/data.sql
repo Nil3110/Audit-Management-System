b871ff6ab371daeb1b017d0edcc60532a62e6edf
Nilotpal Ghosh <g.nilotpal31@outlook.com>
2022-07-14 12:51:17 +0530

    Final

---
+++ Audit-Authorization/target/classes/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO project_manager(id, name, username, password, project_name) VALUES (1, 'Nilotpal Ghosh', 'nil', 'pass123', 'Project-1');
+INSERT INTO project_manager(id, name, username, password, project_name) VALUES (2, 'Sheena Narula', 'sheena', 'sheena123', 'Project-2');
+INSERT INTO project_manager(id, name, username, password, project_name) VALUES (3, 'Rishabh Thakral', 'rishabh', 'rishabh123', 'Project-3');
+INSERT INTO project_manager(id, name, username, password, project_name) VALUES (4, 'Haseeb Raza', 'haseeb', 'haseeb123', 'Project-4');
+
